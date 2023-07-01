33575ba27df8a62e29626843cdf4e1d75c792727
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-01 12:53:24 +0900

    국내근로소득 입력 막음 (자동입력)

--- src/components/TaxCalculator.vue
+++ src/components/TaxCalculator.vue
@@ -4,7 +4,12 @@
       <h2>외납세 간이 계산기</h2>
       <div>
         <h4>준비물</h4>
-        원천징수영수증 <span @click="showModalDownloadGuide = true" style="color: blue; cursor: pointer; font-size: 0.7em">어디서 다운받나요?</span>
+        원천징수영수증
+        <span
+          @click="showModalDownloadGuide = true"
+          style="color: blue; cursor: pointer; font-size: 0.7em"
+          >어디서 다운받나요?</span
+        >
       </div>
       <div>
         <h4>유의사항</h4>
@@ -14,14 +19,14 @@
             달라질 수 있음.
           </li>
           <li>
-            원천징수영수증에 없는 근로소득(해외지급액)은 별도의 종합소득세 신고를 통해 신고하여야 할 수 있음.
+            원천징수영수증에 없는 근로소득(해외지급액)은 별도의 종합소득세
+            신고를 통해 신고하여야 할 수 있음.
           </li>
         </ul>
       </div>
       <v-form @submit.prevent="calculateForeignTaxCredit">
         <v-text-field
           v-model="annualIncome"
-          :value="annualIncome.toLocaleString()"
           label="연간 총급여"
           type="number"
           required
@@ -35,17 +40,16 @@
           required
           @input="validateInput"
           variant="underlined"
-          ></v-text-field
-        >
+        ></v-text-field>
         <v-text-field
           v-model="koreanIncome"
-          label="연간 총급여 중 국내근로소득"
+          label="연간 총급여 중 국내근로소득 (자동입력)"
           type="number"
           required
           @input="validateInput"
           variant="underlined"
-          ></v-text-field
-        >
+          readonly
+        ></v-text-field>
         <v-text-field
           v-model="calculatedTax"
           label="산출세액"
@@ -53,29 +57,33 @@
           required
           @input="validateInput"
           variant="underlined"
-          ></v-text-field
-        >
+        ></v-text-field>
         <v-btn type="submit" color="primary">계산</v-btn>
       </v-form>
-      <div v-if="foreignTaxCredit !== null">
+      <div>
         <h2>외국납부세액공제 적용 가능액</h2>
-        <p>
+        <p v-if="foreignTaxCredit == null">값을 모두 입력하세요.</p>
+        <p v-if="foreignTaxCredit !== null">
           {{ calculatedTax }} / {{ annualIncome }} * {{ foreignIncome }} =
           {{ foreignTaxCredit }}
+          <br />한국에서 동일 국외근로소득에 대해 이미 세금을 냈거나 월급에서
+          원천징수되었다면, 최대
+          <span style="font-weight: 900">{{
+            foreignTaxCredit.toLocaleString()
+          }}</span
+          >원 만큼 세금을 환급받을 수 있습니다.
         </p>
       </div>
 
       <v-dialog v-model="showModalDownloadGuide" width="500">
         <DownloadGuideModal />
       </v-dialog>
-
-
     </v-container>
   </div>
 </template>
 
 <script>
-import DownloadGuideModal from '@/components/DownloadGuideModal.vue';
+import DownloadGuideModal from "@/components/DownloadGuideModal.vue";
 export default {
   components: { DownloadGuideModal },
   data() {
@@ -102,17 +110,18 @@ export default {
       if (this.annualIncome && this.foreignIncome && this.calculatedTax) {
         const foreignTaxCredit =
           (this.calculatedTax / this.annualIncome) * this.foreignIncome;
-        this.foreignTaxCredit = foreignTaxCredit.toFixed(2);
+        this.foreignTaxCredit = foreignTaxCredit.toFixed(0);
       } else {
         this.foreignTaxCredit = null;
       }
     },
-    validateInput(event) {
+    validateInput(e) {
+      console.log(e);
       const regex = /^[0-9]*$/;
-      if (!regex.test(event.target.value)) {
-        event.target.value = event.target.value.slice(0, -1);
+      if (!regex.test(e.target.value)) {
+        e.target.value = e.target.value.slice(0, -1);
       } else {
-        event.target.value = parseInt(event.target.value).toString(); // 숫자로 변환하여 다시 문자열로 반환합니다.
+        e.target.value = parseInt(e.target.value).toString(); // 숫자로 변환하여 다시 문자열로 반환합니다.
       }
     },
   },
