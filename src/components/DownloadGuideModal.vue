64595ed1d62cb0c369d8398d67b53d23fdfd018a
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-02 19:14:55 +0900

    원징 다운로드 가이드 모달 내용 추가

--- src/components/DownloadGuideModal.vue
+++ src/components/DownloadGuideModal.vue
@@ -1,20 +1,34 @@
 <template>
-    <div class="download-guide">
-        다운로드 가이드입니다.
-        <br />
-        홈택스로 이동해서 구구절절
-    </div>
+  <div class="download-guide">
+    [다운로드 가이드]
+    <br />
+    1.
+    <a
+      href="https://www.hometax.go.kr/websquare/websquare.wq?w2xPath=/ui/pp/index.xml&tmIdx=0&tm2lIdx=&tm3lIdx="
+      target="_blank"
+      rel="noopener noreferrer"
+      >홈택스</a
+    >
+    로그인 <br />
+    <img :src="downloadGuide" alt="downloadGuide" /> <br />2. 조회/발급 > 기타
+    조회 >근로소득 지급명세서 조회 > 사업자 번호 클릭
+  </div>
 </template>
 
 <script>
-export default {
+import downloadGuide from "../assets/downloadGuide.png";
 
-}
+export default {
+  data() {
+    return {
+      downloadGuide,
+    };
+  },
+};
 </script>
 
 <style scoped>
 .download-guide {
-    background-color: white;
+  background-color: white;
 }
-
 </style>
