6d4c20a21002e2dc425178d7d409079e0d084e77
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-04 18:34:50 +0900

    계산 기준년도에 따라 윤년을 판별하여 총 일수 반영

--- src/components/ForeignIncomeGuideModal.vue
+++ src/components/ForeignIncomeGuideModal.vue
@@ -21,47 +21,60 @@
         label="계산 기준년도"
         v-model="selectedYear"
         :items="['2023', '2022', '2021', '2020', '2019', '2018']"
-        @input="console.log('년도변경')"
       ></v-combobox>
 
       <v-text-field
         v-model="startDate"
         :label="`${selectedYear}년의 해외근무 시작일 (${selectedYear}년 이전 시작의 경우, ${selectedYear}-01-01)`"
         required
-        @input="console.log('시작일변경')"
         type="date"
       />
       <v-text-field
         v-model="endDate"
-        :label="`${selectedYear}년의 해외근무 종료일`"
+        :label="`${selectedYear}년의 해외근무 종료일 (${selectedYear}년 이후 종료 예정의 경우, ${selectedYear}-12-31)`"
         required
         type="date"
       />
     </v-form>
+    <div>
+      해외근무 일수 | 총 {{ foreignDays }}일 / {{ daysInSelectedYear }}일!!
+    </div>
   </div>
 </template>
 <script>
+import moment from "moment";
 export default {
   data() {
     return {
       annualIncome: this.value,
       selectedYear: 2022,
-      startDate: null,
-      endDate: null,
-    }
+      startDate: "2022-01-01",
+      endDate: "2022-12-31",
+    };
   },
   watch: {
-  selectedYear(newVal) {
-    this.startDate = `${newVal}-01-01`
-    this.endDate = `${newVal}-12-31`
-  }
-},
-
+    selectedYear(newVal) {
+      this.startDate = `${newVal}-01-01`;
+      this.endDate = `${newVal}-12-31`;
+    },
+  },
   mounted() {
     this.annualIncome = this.value;
   },
   props: ["value"],
-  methods: { },
+  methods: {},
+  computed: {
+    foreignDays() {
+      const start = new Date(this.startDate);
+      const end = new Date(this.endDate);
+      const timeDiff = end.getTime() - start.getTime();
+      const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)) + 1;
+      return daysDiff;
+    },
+    daysInSelectedYear() {
+      return moment(String(this.selectedYear)).isLeapYear() ? 366 : 365;
+    },
+  },
 };
 </script>
 <style scoped></style>
